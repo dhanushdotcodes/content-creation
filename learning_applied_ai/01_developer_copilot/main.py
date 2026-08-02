ba9caf3345fce5e102147170cbf547979f249361
dhanushd-27 <dhanushsai2005@gmail.com>
2026-08-02 11:15:28 +0530

    feat: 2 - continous conversations with the ai

--- learning_applied_ai/01_developer_copilot/main.py
+++ learning_applied_ai/01_developer_copilot/main.py
@@ -6,18 +6,51 @@ load_dotenv()
 
 client = OpenAI()
 
-gpt_mini = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
+history = [
         {
             "role": "system",
-            "content": "You are a helpful assistant."
+            "content": "You are a helpful assistant, who tells a random funny knock knock jokes to the user"
+        },
+        {
+            "role": "assistant",
+            "content": "knock knock"
         },
         {
             "role": "user",
-            "content": "Hello, how are you?"
+            "content": "who's there?"
         }
     ]
+
+gpt_mini = client.responses.create(
+    model="gpt-4o-mini",
+    input=history
 )
 
-print(gpt_mini.choices[0].message.content)
+print(gpt_mini.output_text)
+
+history.append({
+    "role": "assistant",
+    "content": gpt_mini.output_text
+})
+
+while True:
+    user_input = input("User: ")
+    
+    if user_input.lower() == "exit" or user_input.lower() == "quit":
+        break
+    
+    history.append({
+        "role": "user",
+        "content": user_input
+    })
+
+    gpt_mini = client.responses.create(
+        model="gpt-4o-mini",
+        input=history
+    )
+
+    print(gpt_mini.output_text)
+    history.append({
+        "role": "assistant",
+        "content": gpt_mini.output_text
+    })
